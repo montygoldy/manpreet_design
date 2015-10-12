14483709a533318923d020d586f064b3ada1e42b
montygoldy <montygoldy2006@gmail.com>
2015-10-12 12:06:36 +0530

    Loop through Post#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
    before_action :find_post, only: [:show, :edit, :update, :destroy]
 
 	def index
-		
+		@posts = Post.all.limit(5).order("created_at desc")
 	end
 
 	def new
